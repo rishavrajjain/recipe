50dda0f897eab3e102181032677593b8079f6d76
Rishav Raj Jain <rishavrajjain@gmail.com>
2025-06-06 13:49:50 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe/firebase_options.dart';
 import 'screens/home_screen.dart'; // Ensure this path is correct
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const CookingAssistantApp());
 }
 
